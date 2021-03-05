f2175504e2f143a3ba267c5a7b3b9e662efa87bf
Sasha Korniichuk <OKorniichuk03@gmail.com>
2021-03-05 19:27:04 +0200

    DAL BLL

---
+++ StoreUI/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿namespace StoreUI
+{
+    using DLL.Entities;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ApplicationContext : DbContext
+    {
+     
+        public ApplicationContext()
+            : base("name=ApplicationContext")
+        {
+        }
+
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Laptop> Laptops { get; set; }
+
+        public DbSet<Laptop> Laptops { get; set; }
+
+    }
+}
